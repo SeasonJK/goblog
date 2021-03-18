804e5a55d2dc4174aa2b73852fc34c0ad4a4a962
Season <1019245592@qq.com>
2021-03-18 15:31:43 +0800

    文章接口模块

--- api/v1/atricle.go
+++ api/v1/atricle.go
@@ -1,15 +1,147 @@
 package v1
 
+import (
+	"github.com/gin-gonic/gin"
+	"goblog/model"
+	"goblog/utils/errmsg"
+	"net/http"
+	"strconv"
+)
 
-//todo 新增文章
+// 新增文章
+func AddArticle(c *gin.Context){
+	var data model.Article
+	_ = c.ShouldBindJSON(data)
 
-//todo 查询分类下所有文章
+	code := model.CreateArticle(&data)
 
-//todo 查询单个文章信息
+	c.JSON(http.StatusOK, gin.H{
+		"status":code,
+		"data":data,
+		"message":errmsg.GetErrMsg(code),
+	})
+}
+// 查询分类下所有文章
+func GetCateArt(c *gin.Context){
+	id, _ := strconv.Atoi(c.Param("id"))
+	pageSize, _ := strconv.Atoi(c.Query("pageSize"))
+	pageNum, _ := strconv.Atoi(c.Query("pageNum"))
 
-//todo 查询文章列表
+	//分页器
+	switch {
+	case pageSize >= 100:
+		pageSize = 100
+	case pageSize <= 0:
+		pageSize = 10
+	}
 
-//todo 编辑文章
+	if pageNum == 0 {
+		pageNum = 1
+	}
 
-//todo 删除文章
+	data, total, code :=model.GetCateArt(id, pageSize, pageNum)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":code,
+		"data":data,
+		"total":total,
+		"message":errmsg.GetErrMsg(code),
+	})
+}
+// 查询单个文章信息
+func GetArtInfo(c *gin.Context){
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	data, code := model.GetArtInfo(id)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":code,
+		"data":data,
+		"message":errmsg.GetErrMsg(code),
+	})
+}
+
+// 查询文章列表
+func GetArtList(c *gin.Context){
+	title := c.Query("title")
+	pageSize, _ := strconv.Atoi(c.Query("pageSize"))
+	pageNum, _ := strconv.Atoi(c.Query("pageNum"))
+
+	//分页器
+	switch {
+	case pageSize >= 100:
+		pageSize = 100
+	case pageSize <= 0:
+		pageSize = 10
+	}
+	if pageNum == 0 {
+		pageNum = 1
+	}
+
+	if len(title) == 0{
+		data, code, total := model.GetArtList()
+		c.JSON(http.StatusOK, gin.H{
+			"status":		code,
+			"data":			data,
+			"total":		total,
+			"message":		errmsg.GetErrMsg(code),
+		})
+		return
+	}
+
+	data, code, total := model.SearchArtTitle(title, pageSize, pageNum)
+	c.JSON(http.StatusOK, gin.H{
+		"status":	code,
+		"data":		data,
+		"total":	total,
+		"message":	errmsg.GetErrMsg(code),
+	})
+}
+// 编辑文章
+func EditArticle(c *gin.Context){
+	var data model.Article
+	id, _ := strconv.Atoi(c.Param("id"))
+	_ = c.ShouldBindJSON(&data)
+
+	code = model.EditArticle(id, &data)
+
+	c.JSON(http.StatusOK, gin.H{
+		"status":	code,
+		"message": 	errmsg.GetErrMsg(code),
+	})
+}
+
+// 删除文章
+func DeleteArt(c *gin.Context){
+	id, _ := strconv.Atoi(c.Param("id"))
+
+	code = model.DeleteArticle(id)
+	c.JSON(http.StatusOK, gin.H{
+		"status":	code,
+		"message":	errmsg.GetErrMsg(code),
+	})
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- model/Article.go
+++ model/Article.go
@@ -1,6 +1,9 @@
 package model
 
-import "gorm.io/gorm"
+import (
+	"goblog/utils/errmsg"
+	"gorm.io/gorm"
+)
 
 type Article struct {
 	gorm.Model
@@ -13,14 +16,111 @@ type Article struct {
 	ReadCount		int	`gorm:"type:int;not null;default:0" json:"read_count"`
 }
 
-//todo 新增文章
+// 新增文章
+func CreateArticle(data *Article) int {
+	err := db.Create(&data).Error
+	if err != nil{
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+// 查询分类下的所有文章
+func GetCateArt(id int, pageSize, pageNum int) ([]Article, int64, int) {
+	var CateArtList []Article
+	var total int64
 
-//todo 查询分类下的所有文章
+	err := db.Select("article.id,title, `cid`, img, created_at, `desc`, comment_count, read_count").Preload("Category").Limit(pageSize).Offset((pageNum-1)*pageSize).Where("cid = ?", id).Find(&CateArtList).Error
+	db.Model(&CateArtList).Where("cid = ?", id).Count(&total)
 
-//todo 查询单个文章
+	if err != nil{
+		return CateArtList, 0, errmsg.ERROR_CATE_NOT_EXIST
+	}
+	return CateArtList, total, errmsg.SUCCESS
+}
+// 查询单个文章
+func GetArtInfo(id int) (Article, int) {
+	var article Article
+	err := db.Where("id = ?", id).Preload("Category").First(&article).Error
+	db.Model(&article).Where("id = ?", id).UpdateColumn("read_count", gorm.Expr("read_count + ?", 1))
 
-//todo 查询文章列表
+	if err != nil{
+		return article, errmsg.ERROR_ART_NOT_EXIST
+	}
+	return article, errmsg.SUCCESS
+}
 
-//todo 编辑文章
+// 查询文章列表
+func GetArtList(title string, pageSize,pageNum int)([]Article, int, int64){
+	var articleList []Article
+	var err error
+	var total int64
 
-//todo 删除文章
+	err = db.Select("article.id, title, img, created_at, updated_at, `desc`, comment_count, read_count, category.name").Limit(pageSize).Offset((pageNum - 1) * pageSize).Order("Created_At DESC").Joins("Category").Find(&articleList).Error
+	// 单独计数
+	db.Model(&articleList).Count(&total)
+	if err != nil {
+		return nil, errmsg.ERROR, 0
+	}
+	return articleList, errmsg.SUCCESS, total
+}
+
+// 查询文章标题
+func SearchArtTitle(title string, pageSize,pageNum int)([]Article, int, int64){
+	var articleList []Article
+	var err error
+	var total int64
+	err = db.Select("article.id,title, img, created_at, updated_at, `desc`, comment_count, read_count, category.name").Limit(pageSize).Offset((pageNum-1)*pageSize).Order("Created_At DESC").Joins("Category").Where("title LIKE ?",
+		title+"%").Find(&articleList).Count(&total).Error
+	if err != nil{
+		return nil, errmsg.ERROR, 0
+	}
+	return articleList, errmsg.SUCCESS, total
+}
+// 编辑文章
+func EditArticle(id int, data *Article) int {
+	var article Article
+	var maps = make(map[string]interface{})
+	maps["title"] = data.Title
+	maps["cid"] = data.Cid
+	maps["description"] = data.Desc
+	maps["content"] = data.Content
+	maps["img"] = data.Img
+
+	err = db.Model(&article).Where("id = ?", id).Updates(&maps).Error
+	if err != nil{
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+
+// 删除文章
+func DeleteArticle(id int) int {
+	var article Article
+	err = db.Where("id = ?", id).Delete(&article).Error
+	if err != nil{
+		return errmsg.ERROR
+	}
+	return errmsg.SUCCESS
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
